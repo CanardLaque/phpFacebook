a3fa366131bf21997afd29b2071958d5832e4d27
vicator6 <Victor@MacBook-Pro-3.local>
2015-11-09 11:55:45 +0100

    Interface quasi finiii
    Ac de l’angular frero

--- vic/assets/js/main.js
+++ vic/assets/js/main.js
@@ -1,34 +1,18 @@
 
-var todolistApp = angular.module('todolistApp', []);
+var phpFacebookApp = angular.module('phpFacebookApp', []);
 
-todolistApp.controller('todoController', ['$scope', function($scope){
+phpFacebookApp.controller('phpFacebookController', ['$scope', function($scope){
 
-    $scope.todos = [
-        { taskName : 'Acheter du pain', done : true},
-        { taskName : 'Réparer le PC' , done : true },
-        { taskName : 'Louer un DVD' , done : false },
-        { taskName : 'Nourrir le chat' , done : false },
-        { taskName : 'Apprendre AngularJS', done : true }
-    ];
+    $scope.addComment = function(){
+        $scope.class = 'show';
 
-    $scope.remaining = function(){
-        var undone = $scope.todos.length;
-
-        $scope.todos.forEach(function(el){
-            if (el.done)
-            undone--;
-        });
-      return undone;
+        $scope.addComment = function(){
+            if ($scope.class === "show")
+                $scope.class = "hide";
+            else
+                $scope.class = "show";
+        };
     };
-
-    $scope.add = function() {
-        $scope.todos.unshift({
-            taskName : $scope.newTaskName,
-            done : false
-        });
-        $scope.newTaskName = '';
-    };
-
 
 }]);
 
