da522a3cf111d54ee6a9d6b651aa45bcc9ef263b
vicator6 <Victor@MacBook-Pro-3.local>
2015-11-11 22:45:10 +0100

    Finish

--- vic/assets/js/main.js
+++ vic/assets/js/main.js
@@ -3,15 +3,10 @@ var phpFacebookApp = angular.module('phpFacebookApp', []);
 
 phpFacebookApp.controller('phpFacebookController', ['$scope', function($scope){
 
-    $scope.addComment = function(){
-        $scope.class = 'show';
+    $scope.visible = false ;
 
-        $scope.addComment = function(){
-            if ($scope.class === "show")
-                $scope.class = "hide";
-            else
-                $scope.class = "show";
-        };
+    $scope.addComment = function (){
+      $scope.visible = !$scope.visible;
     };
 
 }]);
